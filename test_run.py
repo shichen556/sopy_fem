cd3e3e1ed10d77f3981b5c51306058e145868b01
shichen556 <shichenlinxd@gmail.com>
2026-04-04 20:42:56 +0200

    Untrack __pycache/ from all levels, improve readability to 'test_run.py' and 'test_help.py', add a error catch to './sopy_fem/read_data.py' (add to other files as needed) and add comment divisor to './sopy_fem/postprocess.py' for a better readability.

--- test_run.py
+++ test_run.py
@@ -1,11 +1,26 @@
+from os.path import join
 import sys
 from sopy_fem.sopy_fem_run import sopy_fem_run
 
-def test_run():
+def test_run(dataFileName = ""):
     args = sys.argv[1:]
-    dataFileName = ""
     if (len(args)  != 0):
         dataFileName = args[0]
     sopy_fem_run(dataFileName)
 
-test_run()
+'''
+Choose a value for "exampleType" from among the following options to run a example:
+  -  thermal_BR02
+  -  structural_TRUSS02
+  -  structural_FRAME02 
+  -  mechanics_TR03
+  -  mechanics_BR02
+  -  electrical_BR02
+  -  mechanics_QU04
+  -  dynamics_FRAME02
+  -  dynamics_TRUSS02
+'''
+
+exampleType = "structural_FRAME02"
+dataFileName = join("./sopy_fem/Examples/", exampleType, "data.json")
+test_run(dataFileName)
